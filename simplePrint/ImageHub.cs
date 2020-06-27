dbc6d2c422cbd813f45292e44ccf66e86b71172b
Braden Everson <bradeneverson@gmail.com>
2020-06-27 15:23:02 -0500

    SignalR

---
+++ simplePrint/ImageHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simplePrint
+{
+    public class ImageHub : Hub
+    {
+        public async Task sendImage(string user, string imageUrl)
+        {
+            await this.Clients.Caller.SendAsync("receiveImage",user,imageUrl);
+        }
+    }
+}
---
+++ simplePrint/wwwroot/js/image.js
@@ -0,0 +1,25 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/imagehub")
+    .build();
+
+//Receive Image from server, in the end this will return the gcode file
+connection.on("ReceiveMessage", (user, message) => {
+    //Taken from a guide on chat, todo: Change this to the images
+    const msg = message.replace(/&/g, "&").replace(/</g, "<").replace(/>/g, ">");
+    const encodedMsg = user + " :: " + msg;
+    const li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(err => console.error(err.toString()));
+
+//Send Image To Server
+
+document.getElementById("sendImage").addEventListener("click", event => {
+    //Taken from a guide on chat, todo: Change this to the images
+    const user = document.getElementById("userName").value;
+    const message = document.getElementById("userMessage").value;
+    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+});   
